19a58846ff104476cd1a6d925f9e3c34ee3311ed
alberto <vilchezzalberto@gmail.com>
2021-03-14 17:46:28 +0100

    Texto Mana Skill

--- Assets/GUI Kit Casual Game/Scripts/PJ/ManagerSkill.cs
+++ Assets/GUI Kit Casual Game/Scripts/PJ/ManagerSkill.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class ManagerSkill : MonoBehaviour
 {
-   
+    
     public ManagerrBarra ScriptMana;
     // Start is called before the first frame update
     void Start()
